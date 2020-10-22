c3b650fd3a39e5ccf54cbccd577406edb5856b3d
chrysleremmanuel123 <chrysler.emmanuel@gmail.com>
2020-10-22 19:57:55 +0530

    done

--- FENCING THE GROUND/src/index.js
+++ FENCING THE GROUND/src/index.js
@@ -8,15 +8,15 @@
 
 
 // Write the function to retrun the exact length of the rope.
-function find_length(Length,Width){
-
-    return null;
+function find_length(Length, Width) {
+    var perimeter = 2 * (Length + Width)
+    return perimeter;
 }
 
 // Write the function to retrun the exact quantity of carper that is required.
-function find_carper(Length,Width){
-
-    return null;
+function find_carper(Length, Width) {
+    var area = Length * Width
+    return area;
 }
 
 //Use SpecRunner to check the Test Cases.
--- PIRATES SHARE/src/index.js
+++ PIRATES SHARE/src/index.js
@@ -5,21 +5,22 @@
 
 
 //Write a function to return calculate the of JackShare.
-function find_JackShare(goldCoin,X,Y){
-    
-    return null;
+
+function find_JackShare(goldCoin, X, Y) {
+    var jackShare = (X / 100) * goldCoin
+    return jackShare;
 }
 
 
 //Write a function to retrun the  Barbossa share.
-function find_BarbossaShare(goldCoin,X,Y){
-
-    return null;
+function find_BarbossaShare(goldCoin, X, Y) {
+    var barboossaShare = goldCoin - (X / 100) * goldCoin
+    return barboossaShare;
 }
 
 
 //Write a function to retrun the other pirates share.
-function find_OtherShare(goldCoin,X,Y){
-
-    return null;  
+function find_OtherShare(goldCoin, X, Y) {
+    var otherShare = goldCoin - ((X / 100) * goldCoin) + ((Y / 100) * goldCoin)
+    return otherShare;
 }
